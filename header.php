a190acacbdb32f36feb46e1db957cfa24e9e5b69
mxc <matt@curinga.com>
2012-03-15 10:01:28 -0400

    changes to style and header, got things lined up

--- header.php
+++ header.php
@@ -22,31 +22,23 @@
 		<?php do_action( 'bp_before_header' ) ?>
 
 		<div id="header">
-					<h1 id="logo" role="banner"><a href="<?php echo home_url(); ?>" title="<?php _ex( 'Home', 'Home page banner link title', 'buddypress' ); ?>"><?php bp_site_name(); ?></a> :: <?php echo get_bloginfo( 'description' ); ?></h1>
+    		<h1 id="logo" role="banner"><a href="<?php echo home_url(); ?>" title="<?php _ex( 'Home', 'Home page banner link title', 'buddypress' ); ?>"><?php bp_site_name(); ?></a></h1>
+    		<h2 id="tagline">4 children<br />4 society</br>4 the future</h2>
+			<div id="navigation" role="navigation">
+				<?php wp_nav_menu( array( 'container' => false, 'menu_id' => 'nav', 'theme_location' => 'primary', 'fallback_cb' => 'bp_dtheme_main_nav' ) ); ?>
+            </div><!-- navigation -->
 
+    		<div id="search-bar" role="search">
+					<form action="<?php echo bp_search_form_action() ?>" method="post" id="search-form">
+						<label for="search-terms" class="accessibly-hidden"><?php _e( 'Search for:', 'buddypress' ); ?></label>
+						<input type="text" id="search-terms" name="search-terms" value="<?php echo isset( $_REQUEST['s'] ) ? esc_attr( $_REQUEST['s'] ) : ''; ?>" />
+						<?php echo bp_search_form_type_select() ?>
+						<input type="submit" name="search-submit" id="search-submit" value="<?php _e( 'Search', 'buddypress' ) ?>" />
+    					<?php wp_nonce_field( 'bp_search_form' ) ?>
+					</form><!-- #search-form -->
 
-				<div id="navigation" role="navigation">
-					<?php wp_nav_menu( array( 'container' => false, 'menu_id' => 'nav', 'theme_location' => 'primary', 'fallback_cb' => 'bp_dtheme_main_nav' ) ); ?>
-
-
-			<div id="search-bar" role="search">
-						<form action="<?php echo bp_search_form_action() ?>" method="post" id="search-form">
-							<label for="search-terms" class="accessibly-hidden"><?php _e( 'Search for:', 'buddypress' ); ?></label>
-							<input type="text" id="search-terms" name="search-terms" value="<?php echo isset( $_REQUEST['s'] ) ? esc_attr( $_REQUEST['s'] ) : ''; ?>" />
-
-							<?php echo bp_search_form_type_select() ?>
-
-							<input type="submit" name="search-submit" id="search-submit" value="<?php _e( 'Search', 'buddypress' ) ?>" />
-
-							<?php wp_nonce_field( 'bp_search_form' ) ?>
-
-						</form><!-- #search-form -->
-
-				<?php do_action( 'bp_search_login_bar' ) ?>
-			</div><!-- #search-bar -->
-
-        </div><!-- navigation -->
-
+	    		<?php do_action( 'bp_search_login_bar' ) ?>
+    		</div><!-- #search-bar -->
 
 			<?php do_action( 'bp_header' ) ?>
 
